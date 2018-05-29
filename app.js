9e83f4386f73f7c8df091dad879faa59467c3079
Thomas Liu <thomas.liu01@sap.com>
2018-05-29 17:15:17 +0800

    Head count function

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 App({
-  onLaunch: function () {
-    console.log('App Launch')
+  onLaunch: function() {
+    console.log('App Launch');
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        var latitude = res.latitude // 经度
+        var longitude = res.longitude // 纬度
+        console.log("User Location: " + latitude + "   " + longitude);
+      },
+      error: (res) => {
+        debugger;
+      }
+    })
+
   },
-  onShow: function () {
+  onShow: function() {
     console.log('App Show')
   },
-  onHide: function () {
+  onHide: function() {
     console.log('App Hide')
   },
   globalData: {
